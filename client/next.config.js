e8b6f87bd36ad4bacbf652baee5f00b9c837b547
kalePrashant <prashantkale2208@gmail.com>
2025-05-08 16:41:19 +0530

    deply error resolved

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
   },
+  basePath: '/TradeShiksha',
+  assetPrefix: '/TradeShiksha/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
--- client/src/pages/SomePage.js
+++ client/src/pages/SomePage.js
@@ -1,10 +1,13 @@
-// ... existing code ...
-return (
-  <>
-    <div className={styles.container}>
-      {/* Your page content */}
-    </div>
-    {/* Remove any <Footer /> component from here */}
-  </>
-);
-// ... existing code ...
+import React from 'react';
+import styles from '../styles/SomePage.module.css';
+
+export default function SomePage() {
+  return (
+    <>
+      <div className={styles.container}>
+        {/* Your page content */}
+      </div>
+      {/* Remove any <Footer /> component from here */}
+    </>
+  );
+};
